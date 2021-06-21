2ac6e80e611c55dcf8749386073b27a802a00516
Natalya Lisitsina@ <lisicina_natasha@bk.ru>
2021-06-22 00:18:00 +0400

    work

--- 1.py
+++ 1.py
@@ -1,12 +1,61 @@
 import requests
-import vk
+from pprint import pprint
 
-url = 'https://vk.com/dev/photos.get'
-params = {'owner_id':'552934290', 'v': '5.131',
-          'access token':
-          '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008',
+TOKEN = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
+USER_ID = '552934290'
+token_yd = 'AQAAAABVR-QsAAcnmDgIDf2r-Ex-mIXUwvUHxQA'
+
+url = 'https://api.vk.com/method/photos.get'
+params = {'owner_id':USER_ID,
+          'v': '5.77',
+          'access_token': TOKEN,
           'album_id':'profile',
           'extended': '1',
           'photo_sizes':'1'}
 resp = requests.get(url=url, params=params)
-print(resp.json())
+if resp.status_code != 200:
+    raise Exception("Unable to get photos")
+photos = resp.json()['response']['items']
+
+
+def get_upload(file, token_yd):
+    url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+    params = {"path": file, "fields": "json", "overwrite": "true"}
+    headers = {'Accept': 'application/json', 'Authorization': f'OAuth {token_yd}'}
+    resp = requests.get(url, params=params, headers=headers)
+    href = resp.json()['href']
+    return href
+
+
+def get_biggest_px (size_dict):
+    if size_dict['width']>= size_dict['height']:
+        return size_dict['width']
+    else:
+        return size_dict['height']
+
+def download_photo(url):
+    r = requests.get(url)
+    filename = url.split('?')[0].split('/')[-1]
+    with open(filename, 'bw') as file:
+        for chuk in r.iter_content(4096):
+            file.write(chuk)
+
+for photo in photos:
+
+    sizes = photo['sizes']
+    max_size_url = max(sizes, key=get_biggest_px)['url']
+    max_size_type = max(sizes, key=get_biggest_px)['type']
+    file_name = max_size_url.split('?')[0].split('/')[-1]
+    download_photo(max_size_url)
+    url = get_upload(file_name,'AQAAAABVR-QsAAcnmDgIDf2r-Ex-mIXUwvUHxQA')
+    params = {'path': f"Netologe/f'{file_name}'", 'fields': 'json'}
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth {token_yd}'}
+    resp = requests.put(url, data=open(file_name, 'rb'))
+    resp.raise_for_status()
+    if resp.status_code == 201:
+        print('success')
+
+
+
+
+
